2b7eb30ccd620ac76fb2c5724179611f3a7b4351
30Piraten <kioyarautenberg@gmail.com>
2023-11-09 22:19:47 +0200

    updated file name to case and print statement

---
+++ array_of_seq/case.py
@@ -0,0 +1,9 @@
+names = [
+    ("gary", "x", "yara", "baldwin", "spence", "kubie")
+]
+
+
+for n in names:
+    match n:
+        case [name, _, _, *rest] if n is not None:
+            print(f"{name:4} | {rest}")
